0e9894d7c5a70c14df8189f44656d03edc2dd7e9
Justin Zuniga <justinzu93@gmail.com>
2021-01-09 18:51:27 -0600

    cors

--- app.js
+++ app.js
@@ -69,21 +69,7 @@ app.use("/images", express.static(path.join("/public/images")));
 app.use(express.json({ extented: false }));
 app.use(cookieParser());
 app.use(
-  cors({
-    
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
+  cors()
 );
 app.use(passport.initialize());
 app.use(compression());
