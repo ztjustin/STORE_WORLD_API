5753066795a3e2ba0be2ae01a974602e34ab9dfb
Justin Zuniga <justinzu93@gmail.com>
2021-01-09 18:19:10 -0600

    test

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
  * @returns {string} token - decrypted token
  */
 const jwtExtractor = (req) => {
-  console.log(req.cookies);
+  console.log(req.cookies["__Secure-next-auth.session-token"]);
   let token = null;
   if (req.headers.authorization) {
     token = req.headers.authorization.replace("Bearer ", "").trim();
