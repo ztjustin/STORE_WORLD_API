31afd00ebc95f77f870a4b33dbb15ca3046d3962
Justin Zuniga <justinzu93@gmail.com>
2021-01-10 22:49:26 -0600

    images named changes

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,9 +1,9 @@
-const model = require('../models/product')
-const uuid = require('uuid')
-const { matchedData } = require('express-validator')
-const utils = require('../middleware/utils')
-const db = require('../middleware/db')
-const emailer = require('../middleware/emailer')
+const model = require("../models/product");
+const uuid = require("uuid");
+const { matchedData } = require("express-validator");
+const utils = require("../middleware/utils");
+const db = require("../middleware/db");
+const emailer = require("../middleware/emailer");
 
 /*********************
  * Private functions *
@@ -12,12 +12,11 @@ const emailer = require('../middleware/emailer')
 /**
  * Creates a new item in database
  * @param {Object} req - request object
-*/
-const createItem = async (req,res) => {
+ */
+const createItem = async (req, res) => {
   return new Promise(async (resolve, reject) => {
-
-    urlPath = 'https://cr-auto.herokuapp.com';
-    const urlImagesArray = await getArrayUrlImages(req.files,urlPath);
+    urlPath = "https://cr-auto.herokuapp.com";
+    const urlImagesArray = await getArrayUrlImages(req.files, urlPath);
 
     const product = new model({
       model: req.body.model,
@@ -43,71 +42,67 @@ const createItem = async (req,res) => {
       featured: req.body.featured,
       user: req.body.user,
       urlImages: urlImagesArray,
-      equipment: req.body.equipment
-  
-    })
+      equipment: req.body.equipment,
+    });
     product.save((err, item) => {
-      
       if (err) {
-        reject(utils.buildErrObject(422, err.message))
+        reject(utils.buildErrObject(422, err.message));
       }
 
-      resolve(item)
-
-    })
-  })
-}
-
+      resolve(item);
+    });
+  });
+};
 
 /********************
  * Get Array Images *
  ********************/
 
-const getArrayUrlImages = (files,urlPath) => {
+const getArrayUrlImages = (files, urlPath) => {
   let array = [];
 
-  if(files.images){
-    files.images.map((file)=> {
-      let urlImage = urlPath + '/images/' + file.filename;
+  if (files.images) {
+    files.images.map((file) => {
+      let urlImage = urlPath + "/images/" + file.filename;
       array.push({ url: urlImage });
-    })
+    });
   }
 
   return array;
-
-}
+};
 
 /********************
  * Multer functions *
  ********************/
 
-const multer = require("multer")
+const multer = require("multer");
 
 const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpeg'
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
 };
 
-const storage = multer.diskStorage({ 
-  destination: (req,file,cb) => {
-      const isValid = MIME_TYPE_MAP[file.mimetype];
-      let error = new Error("Invalid Type");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid Type");
 
-      if(isValid){
-        error = null
-      }
-      cb(error,"public/images");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "public/images");
   },
   filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join('-');
+    const name = file.originalname.toLowerCase().split(" ").join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + '.' + ext);
+    cb(null, name + "-" + Date.now() + + "-" + uuid()  +"." + ext);
+  },
+});
 
-  } 
-})
-
-exports.upload = multer({ storage: storage }).fields([{ name: 'images', maxCount: 5 }])
+exports.upload = multer({ storage: storage }).fields([
+  { name: "images", maxCount: 5 },
+]);
 
 /********************
  * Public functions *
@@ -120,13 +115,15 @@ exports.upload = multer({ storage: storage }).fields([{ name: 'images', maxCount
  */
 exports.getItems = async (req, res) => {
   try {
-    const query = await db.checkQueryString(req.query)
-    res.status(200).json(await db.getItems(req, model, query, ["equipment", "brand"]))
+    const query = await db.checkQueryString(req.query);
+    res
+      .status(200)
+      .json(await db.getItems(req, model, query, ["equipment", "brand"]));
     // res.status(200).json(await getAllFeaturedsItems())
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Get items function called by route
@@ -135,12 +132,12 @@ exports.getItems = async (req, res) => {
  */
 exports.getnewItems = async (req, res) => {
   try {
-    const query = await db.checkQueryStringDates(req.query)
-    res.status(200).json(await db.getItems(req, model, query))
+    const query = await db.checkQueryStringDates(req.query);
+    res.status(200).json(await db.getItems(req, model, query));
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Get item function called by route
@@ -149,13 +146,13 @@ exports.getnewItems = async (req, res) => {
  */
 exports.getItem = async (req, res) => {
   try {
-    req = matchedData(req)
-    const id = await utils.isIDGood(req.id)
-    res.status(200).json(await db.getItem(id, model))
+    req = matchedData(req);
+    const id = await utils.isIDGood(req.id);
+    res.status(200).json(await db.getItem(id, model));
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Update item function called by route
@@ -164,19 +161,19 @@ exports.getItem = async (req, res) => {
  */
 exports.updateItem = async (req, res) => {
   try {
-    req = matchedData(req)
-    const id = await utils.isIDGood(req.id)
+    req = matchedData(req);
+    const id = await utils.isIDGood(req.id);
     const doesEmailExists = await emailer.emailExistsExcludingMyself(
       id,
       req.email
-    )
+    );
     if (!doesEmailExists) {
-      res.status(200).json(await db.updateItem(id, model, req))
+      res.status(200).json(await db.updateItem(id, model, req));
     }
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Create item function called by route
@@ -185,8 +182,8 @@ exports.updateItem = async (req, res) => {
  */
 exports.createItem = async (req, res) => {
   try {
-    const item = await createItem(req,res)
-    res.status(201).json(item)
+    const item = await createItem(req, res);
+    res.status(201).json(item);
     // if(!req.files['images']){
     //   res.status(422).json({
     //     'errors' : {
@@ -204,9 +201,9 @@ exports.createItem = async (req, res) => {
     //   res.status(201).json(item)
     // }
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Delete item function called by route
@@ -215,27 +212,31 @@ exports.createItem = async (req, res) => {
  */
 exports.deleteItem = async (req, res) => {
   try {
-    req = matchedData(req)
-    const id = await utils.isIDGood(req.id)
-    res.status(200).json(await db.deleteItem(id, model))
+    req = matchedData(req);
+    const id = await utils.isIDGood(req.id);
+    res.status(200).json(await db.deleteItem(id, model));
   } catch (error) {
-    utils.handleError(res, error)
+    utils.handleError(res, error);
   }
-}
+};
 
 /**
  * Gets all items from database
  */
 const getAllFeaturedsItems = async () => {
   return new Promise((resolve, reject) => {
-    model.find(
-      { featured: true }, { }, { sort: { 'createdAt' : -1 } },
-      (err, items) => {
-        if (err) {
-          reject(utils.buildErrObject(422, err.message))
+    model
+      .find(
+        { featured: true },
+        {},
+        { sort: { createdAt: -1 } },
+        (err, items) => {
+          if (err) {
+            reject(utils.buildErrObject(422, err.message));
+          }
+          resolve(items);
         }
-        resolve(items)
-      }
-    ).limit( 2 )
-  })
-}
+      )
+      .limit(2);
+  });
+};
